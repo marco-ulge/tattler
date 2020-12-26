739d5e3a26c6f1365e47045fd5805c021b0d5970
marcoulge <marco.ulgelmo@gmail.com>
2020-12-26 22:30:59 +0100

    [HORROR] Gods of programming please forgive me

--- utility/results.py
+++ utility/results.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from utility import utilities as utils
 
 
 def write_text_header(output_file, message):
@@ -17,7 +16,7 @@ def write_text_header(output_file, message):
                 "a+",
                 encoding="utf-8"
                 ) as output_file:
-            output_file.write(message + utils.concatenate_char("\n", 2))
+            output_file.write(message + "\n" * 2)
 
     except FileNotFoundError:
         sys.exit("File not found: " + output_file.name + ". Aborting.")
@@ -27,7 +26,7 @@ def write_text_header(output_file, message):
 
 def write_text_footer(output_file, total_issues, total_files, execution_time):
     """Writes the file footer
-    
+
     Arguments:
         output_file {string} -- the file (path) to save to
         total_issues {int} -- the overall number of issues found
@@ -55,7 +54,7 @@ def write_text_footer(output_file, total_issues, total_files, execution_time):
 def store_results_text(blacklist_results, properties_file_results,
                        single_string_results, output_file, filename):
     """Stores the analysis results within a file
-    
+
     Arguments:
         blacklist_results {list} -- list of string that matched blacklist_rules
         single_string_results {list} -- list of single strings determined to be dangerous
@@ -72,25 +71,25 @@ def store_results_text(blacklist_results, properties_file_results,
             output_file.write("Results for File: " + str(filename) + "\n")
 
             if blacklist_results:
-                output_file.write(utils.concatenate_char("\n", 2))
+                output_file.write("\n" * 2)
                 output_file.write("BLACKLIST RESULTS:\n")
                 for result in blacklist_results:
                     output_file.write("Line number: " + str(result.line_number) + "\n")
                     output_file.write("Occurrence: " + result.occurrence)
                     output_file.write("Rule: " + result.rule)
-                    output_file.write(utils.concatenate_char("\n", 2))
+                    output_file.write("\n" * 2)
 
             if properties_file_results:
-                output_file.write(utils.concatenate_char("\n", 2))    
+                output_file.write("\n" * 2)
                 output_file.write("PROPERTIES FILE RESULTS:\n")
                 for result in properties_file_results:
                     output_file.write("Line number: " + str(result.line_number) + "\n")
                     output_file.write("Type: " + result.specific_string_occurrence + "\n")
                     output_file.write("Occurrence: " + result.occurrence.lstrip() + "\n")
-                    output_file.write(utils.concatenate_char("\n", 2))
+                    output_file.write("\n" * 2)
 
             if single_string_results:
-                output_file.write(utils.concatenate_char("\n", 2))    
+                output_file.write("\n" * 2)
                 output_file.write("SINGLE STRING RESULTS:\n")
                 for result in single_string_results:
                     output_file.write("Line number: " + str(result.line_number) + "\n")
@@ -98,10 +97,10 @@ def store_results_text(blacklist_results, properties_file_results,
                     output_file.write("Occurrence: " + result.occurrence + "\n")
                     output_file.write("Shannon Entropy: " + str(result.shannon_entropy) + "\n")
                     output_file.write("Password blacklist match: " + str(result.blacklist_match))
-                    output_file.write(utils.concatenate_char("\n", 2))
+                    output_file.write("\n" * 2)
 
-            output_file.write(utils.concatenate_char("=", 100))
-            output_file.write(utils.concatenate_char("\n", 2))
+            output_file.write("=" * 100)
+            output_file.write("\n" * 2)
 
     except FileNotFoundError:
         sys.exit("File not found: " + output_file.name + ". Aborting.")
@@ -130,3 +129,4 @@ def store_exceptions_text(output_file, data, message=None):
                 output_file.write("File: " + elem + "\n")
     except IOError:
         sys.exit("Error opening file: " + output_file.name + ". Aborting.")
+
